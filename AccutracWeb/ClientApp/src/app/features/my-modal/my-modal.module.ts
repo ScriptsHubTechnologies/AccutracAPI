a90b39328d2e7341873ed8afff2d57fc39cbfa08
Kiran Irabatti <kiran.irabatti@ace.edu>
2022-08-29 18:26:18 +0530

    PDF viewer added - content not yet visible in PDF

--- AccutracWeb/ClientApp/src/app/features/my-modal/my-modal.module.ts
+++ AccutracWeb/ClientApp/src/app/features/my-modal/my-modal.module.ts
@@ -14,6 +14,7 @@ import { PdfViewerModule } from 'ng2-pdf-viewer';
   imports: [
     CommonModule,
     PdfViewerModule,
+    MatDialogModule
   ],
   exports: [FormsModule, MatDialogModule, MatFormFieldModule, MatButtonModule, MatInputModule]
 })
